d298656a6c4db6a5182cfecb1000259bb06d0509
Atharva <atharvasrivastava79@gmail.com>
2025-03-16 13:56:37 +0530

    3_usrinput - complete

---
+++ exercises/area_calc.py
@@ -0,0 +1,8 @@
+print("Hello!")
+
+width = float(input("Please enter the width of the rectangle: "))
+height = float(input("Please enter the height of the rectangle: "))
+
+area = width * height
+
+print(f"The area of the rectangle is {area}unit².")
---
+++ learning/3_usrinput.py
@@ -0,0 +1,2 @@
+# the input() function prompts the user to enter data into the cli
+# returns the input as a string
