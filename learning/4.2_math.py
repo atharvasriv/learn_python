c514fa8882cdc512ec496d52ad0f488da0d055d9
Atharva <atharvasrivastava79@gmail.com>
2025-03-17 17:37:39 +0530

    math functions -2

---
+++ learning/4.2_math.py
@@ -0,0 +1,28 @@
+import math as mt
+
+# global functions
+
+# round(x, n) - rounds the value of x to n decimal places
+# round(x) - rounds to nearest int
+# abs() - gives the absolute value of the num
+# pow(x, y) - raises x to y
+# max(x_1, x_2, x_3, ..., x_n) - gives the maximum value of x_i where i = 1 to n
+
+x = 2
+y = -6
+z = 2.71828
+
+print(round(z)) # outputs 3
+print(abs(y)) # outputs 6
+print(pow(y, x)) # outputs 36
+print(max(x, abs(y), z)) # outputs 6
+
+# some math functions
+# math.pi, math.e - value of pi and e respectively
+# math.sqrt() - square root of a number
+# math.ceil() - ceiling function
+# math.floor() - floor function
+
+print(mt.ceil(9.1)) # outputs 10
+print(mt.floor(9.9)) # outputs 9
+print(mt.sqrt(9)) # outputs 3.0
